570b96699f959e8d6bad4030c5c09d6703c0d2e5
Luis Reyes <luisreyesxv@yahoo.com>
2020-08-27 00:57:17 -0400

    Done.

--- src/components/EyesOnMe.js
+++ src/components/EyesOnMe.js
@@ -1,1 +1,32 @@
 // Code EyesOnMe Component Here
+// Code Keypad Component Here
+import React from 'react'
+
+
+class EyesOnMe extends React.Component {
+
+
+    focusHandler = () => {
+        console.log("Good!")
+
+    }
+
+    blurHandler = () => {
+        console.log("Hey! Eyes on me!")
+
+    }
+
+
+
+    render(){
+        return(
+        
+        <button onFocus ={this.focusHandler} onBlur = {this.blurHandler}>Luis </button>
+        )
+    }
+
+
+
+}
+
+export default EyesOnMe
